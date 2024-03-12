a1bcbdd75587f2706464841f88941d615c1b4ca5
daniel-dabbah <115877882+daniel-dabbah@users.noreply.github.com>
2024-03-12 15:17:34 +0200

    Update README.py

--- README.py
+++ README.py
@@ -1,10 +1,10 @@
-Created on Tue Feb 27 10:30:49 2024
-
+# Sentiment Analysis using LSTM models
 In this project I Conducted sentiment analysis on the Stanford Sentiment Treebank, employing PyTorch and LSTM layers.
 Developed and implemented training code from scratch to create multiple models, subsequently conducting comprehensive
 comparisons for enhanced performance evaluation.
 
-![image](https://github.com/daniel-dabbah/Sentiment-Analysis-LSTM/assets/115877882/afbb4108-8a79-4ce3-bf0c-98607bb72429)
+![image](https://github.com/daniel-dabbah/Sentiment-Analysis-LSTM/assets/115877882/dcf3f05b-f965-4c96-b157-577a3a160504)
+
   
   
 
